014c12d76dd76e7d338aba63598522035ece6cf3
이여진 <yeojinlee1020@gmail.com>
2025-01-23 20:43:55 +0900

    feat: 가게명으로 가게ID 조회 API 구현

---
+++ src/main/java/clovar/howkiki/domain/store/dto/request/StoreIdRequestDto.java
@@ -0,0 +1,11 @@
+package clovar.howkiki.domain.store.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class StoreIdRequestDto {
+    private String storeName;
+
+}
